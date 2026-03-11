2ddf9f279fd471856f3e9d76340bb7a375fbe871
ThirumaleshFSD <sirisampadha939184@gmail.com>
2026-03-11 07:57:26 +0530

    1st problem in leetcode

---
+++ LeetCode2520.java
@@ -0,0 +1,22 @@
+// 2520. Count the Digits That Divide a Number
+public class LeetCode2520 {
+    public int countDigits(int num) {
+        int count =0;
+        int orginalNum =num;
+        while(num>0){
+            int rem = num%10;
+            num/=10;
+            if(rem!=0 && orginalNum % rem==0){
+                count++;
+            }
+            
+        }
+        return count;
+}
+public static void main(String[] args) {
+    LeetCode2520 obj = new LeetCode2520();
+    int num = 121;
+    int result = obj.countDigits(num);
+    System.out.println(result); 
+}
+}
